a5ef14de86add175c74bdb0b54d248ce2d44f9ef
SQ <shamyrquishpet@gmail.com>
2024-11-11 16:13:21 -0500

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+    provideFirebaseApp(() => initializeApp({apiKey: "AIzaSyDp4RCs8UwhBrTNivTVoBkbDwQ_s1LYxLI",
+      authDomain: "b-1accc.firebaseapp.com",
+      projectId: "b-1accc",
+      storageBucket: "b-1accc.firebasestorage.app",
+      messagingSenderId: "606986248368",
+      appId: "1:606986248368:web:df6c0a16fa5bc4ed9a9b75",
+      measurementId: "G-QDN8GEEH17"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+
+interface AuthCredentials {
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  constructor(private auth: Auth) { }
+
+  async register({ email, password }: AuthCredentials) {
+    try {
+      const user = await createUserWithEmailAndPassword(this.auth, email, password);
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async login({ email, password }: AuthCredentials) {
+    try {
+      const user = await signInWithEmailAndPassword(this.auth, email, password);
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  logout() {
+    return signOut(this.auth);
+  }
+}
