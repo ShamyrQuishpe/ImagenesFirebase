617ea7060da0848c9ac4e487689981cdd599c0df
Monica Jaña <monicajana1804@gmail.com>
2024-11-13 14:03:23 -0500

    correcion app-moduls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { getStorage, provideStorage } from '@angular/fire/storage';
       storageBucket: "movil24b.firebasestorage.app",
       messagingSenderId: "401408439008",
       appId: "1:401408439008:web:8cfee98688bf1e78205685"
-     })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()), provideFirebaseApp(() => initializeApp({"projectId":"proyectom-21417","appId":"1:123108399587:web:115a477852cd4d7d1428d6","storageBucket":"proyectom-21417.firebasestorage.app","apiKey":"AIzaSyDQBcguYwkJZKL5aSIeX93NHkf8UbOzLN8","authDomain":"proyectom-21417.firebaseapp.com","messagingSenderId":"123108399587","measurementId":"G-2DT69V8DHK"})), provideFirebaseApp(() => initializeApp({"projectId":"movil24b","appId":"1:401408439008:web:8cfee98688bf1e78205685","storageBucket":"movil24b.firebasestorage.app","apiKey":"AIzaSyB4n_LqqTgsBbO0Qd3_7DnwQUVrM7jLPo4","authDomain":"movil24b.firebaseapp.com","messagingSenderId":"401408439008"}))],
+     })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()),],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
